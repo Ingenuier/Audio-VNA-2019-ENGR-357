a34d6d0150390390b0029bfbb5c6ce9ec061bf77
Ingenuier <generaltimelord@gmail.com>
2019-06-06 20:11:34 -0700

    Changes to VNAMain

--- Python/VNAMain.py
+++ Python/VNAMain.py
@@ -11,10 +11,10 @@ import time
 # fs is sampling frequencies
 fs = 96000
 # in seconds
-T = 1/fs
+T = 1 / fs
 # pick frequenies to start at 2/(NT) where N is number of points wanting to be tested
 N = 9000
-frequencies = np.linspace(2/(N*T), (1875)/(N*T), num=35)
+frequencies = np.linspace(2 / (N * T), (1875) / (N * T), num=35)
 timeToSample = 1
 # Amplitude of sample
 sendAmplitude = .25
@@ -27,7 +27,7 @@ niter = 0
 refArray = []
 
 # reflectArray is the reference array that should hold the V- values
-reflectArray = [] 
+reflectArray = []
 
 for freq in frequencies:
     # TODO: run fft in every for loop on both left and right channel. 
@@ -40,78 +40,106 @@ for freq in frequencies:
     # TODO: Fix that issue cuase it's stupid
     # np.arange(start, stop, step, dtype = none)
     t = np.arange(0, timeToSample, 1 / fs)
-    # data = sendAmplitude * np.sin(2 * np.pi * freq * t)   
-    data = np.array([np.array([1,1]) * sendAmplitude * np.sin(2 * np.pi * freq * m) for m in t])
-    
+
+    # data = sendAmplitude * np.sin(2 * np.pi * freq * t)
+    data = np.array([np.array([1, 1]) * sendAmplitude * np.sin(2 * np.pi * freq * m) for m in t])
+
     # playrec plays and records data
     recData = sd.playrec(data, fs, channels=2)
     # print(recData)
+
     # will only display graph when recording is done?
     sd.wait()
 
-    # Assume for now that left channel is reflected bridge    
+    # FIXME: left channel has a hard time recording data
+    # possibility it is a hardware issue
+    # Assume for now that left channel is reflected bridge
     # left channel
-    leftData = recData[:,0]
-    leftInput = data[:,0]
+    leftData = recData[:, 0]
+    leftInput = data[:, 0]
+
     # This will get highest value in the sin wave
     recordAmplitude = np.amax(leftData)
+
     # Find first instance where recorded is 50% of it's amplitude
     first = np.argmax(leftData > .5 * recordAmplitude)
+
     # Find first instance where generated is 50% of it's amplitude
     # Assume generated is all the same
     firstGen = np.argmax(leftInput > .5 * sendAmplitude)
+
     # This is where the recorded data is being chopped
-    leftData = leftData[first:]    
-    # Get wanted length of thehow to make matlab plots run simultaneous generated
+    leftData = leftData[first:]
+
+    # Get wanted length of the how to make matlab plots run simultaneous generated
     wantedLength = len(leftData)
-    leftInput = data[:,0][firstGen:firstGen + wantedLength]
+    leftInput = data[:, 0][firstGen:firstGen + wantedLength]
     lx = t[firstGen:firstGen + wantedLength]
+
+    # plot wave form against what is sent
+    # testing code
+    plt.figure(1)
+    plt.plot(lx, leftData, label='recorded')
+    plt.plot(lx, leftInput, label='sent')
+    plt.legend(loc="lower left")
+    plt.title('Left Wave Data')
 
     # Assume right channel is reference channel
     # right channel
-    rightData = recData[:,1]
-    rightInput = data[:,1]
+    rightData = recData[:, 1]
+    rightInput = data[:, 1]
+
     # This will get highest value in the sin wave
     recordAmplitude = np.amax(rightData)
+
     # Find first instance where recorded is 50% of it's amplitude
     first = np.argmax(rightData > .5 * recordAmplitude)
+
     # Find first instance where generated is 50% of it's amplitude
     # Assume generated is all the same
     firstGen = np.argmax(rightInput > .5 * sendAmplitude)
+
     # This is where the recorded data is being chopped
-    rightData = rightData[first:]    
+    rightData = rightData[first:]
+
     # Get wanted length of the generated
     wantedLength = len(rightData)
-    rightInput = data[:,1][firstGen:firstGen + wantedLength]
+    rightInput = data[:, 1][firstGen:firstGen + wantedLength]
     rx = t[firstGen:firstGen + wantedLength]
 
+    # plot wave form against what is sent
+    # testing code
+    plt.figure(2)
+    plt.plot(rx, rightData, label='recorded')
+    plt.plot(rx, rightInput, label='sent')
+    plt.legend(loc="lower left")
+    plt.title('Right Wave Data')
+
     # This block of code is to plot the waveforms
-    # Discount osciloscope
-    plt.figure(1)
-    plt.plot(lx, leftData, label='reflection')
-    plt.plot(rx, rightData, label='reference')
+    # Discount oscilloscope
+    plt.figure(3)
+    plt.plot(lx, leftData, label='reference')
+    plt.plot(rx, rightData, label='reflected')
     plt.legend(loc="lower left")
     plt.title('Reference vs Reflected')
-    plt.show()
 
-
-    # TODO: figure out way to grab only the frequency value that I want   
+    # TODO: figure out way to grab only the frequency value that I want
     # Fast Fourier Transform
-    
-    # fftshift should shift off the dc 
-    reference = np.fft.fftshift(np.fft.fft(rightData/N))
-    reflection = np.fft.fftshift(np.fft.fft(leftData/N))
 
-    # The frequency plotter for the fft does not work
+    # fftshift should shift off the dc
+    reference = np.fft.fftshift(np.fft.fft(leftData / N))
+    reflection = np.fft.fftshift(np.fft.fft(rightData / N))
+
+    # FIXME: The frequency plotter for the fft does not work
     # no clue why, but it was only used for testing so not too worried
     # plt.plot(frequencies,np.abs(reference))
     # plt.show()
 
     # getting rid of the imaginary part of all elements
-    
+
     # getting the only value that matters in the fft array
-    reference = reference[1+niter]
-    # getting the magnitude 
+    reference = reference[1 + niter]
+    # getting the magnitude
     refMax = np.abs(reference)
 
     # refMax = np.amax(refMax)
@@ -124,7 +152,7 @@ for freq in frequencies:
     print(refArray)
 
     # getting the only value that matters in the fft array
-    reflection = reflection[1+niter] 
+    reflection = reflection[1 + niter]
     # getting rid of the imaginary part of all elements
     reflectMax = np.abs(reflection)
 
@@ -143,20 +171,21 @@ for freq in frequencies:
     niter += 1
     break
 
-
 # reflection/reference angle reflection-reference magnitude
-#plt.plot(frequencies, magnitudes)
-#plt.show()
+# plt.plot(frequencies, magnitudes)
+# plt.show()
 
-gamma = reflectMax/refMax
+# TODO: do the calibration of open, short, and ZL of 620
+# TODO: average the reflectarray and refarray to be used here
+gamma = reflectMax / refMax
 print("gamma = "),
 print(gamma)
 
 # last few lines are used as a discount smith chart
 # This value is fixed to the board
-Z0 = 1000
+Z0 = 620
 
-ZL = -((gamma+1)*Z0)/(gamma-1)
+ZL = -((gamma + 1) * Z0) / (gamma - 1)
 print("ZL = "),
 print(ZL)
-
+plt.show()
